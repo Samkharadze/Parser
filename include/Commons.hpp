ceec25166cec704e39a892593256dd932a375e7f
Samkharadze <giorgisamh1771@gmail.com>
2019-10-13 23:40:54 +0300

    oks

--- include/Commons.hpp
+++ include/Commons.hpp
@@ -9,4 +9,4 @@ int findend(const std::string& s, int startInd, char openSym, char closeSym);
 int missspaces(const std::string&s, int current);
 std::string getstring(const std::string &s, int start);
 bool isnum(char ch);
-std::pair<double, int> getnumwithlen(const std::string &s, int start); 
+std::pair<double, int> getnumwithlen(const std::string &s, int start);
