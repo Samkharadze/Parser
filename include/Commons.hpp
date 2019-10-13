e3c3a3f384a51b0912c2abb62cc66f8eaac0ac5d
Samkharadze <giorgisamh1771@gmail.com>
2019-10-13 23:00:45 +0300

    Letters

--- include/Commons.hpp
+++ include/Commons.hpp
@@ -4,9 +4,9 @@
 
 
 //ok
-bool IsSpace(char ch);
-int FindEnd(const std::string& s, int startInd, char openSym, char closeSym);
-int MissSpaces(const std::string&s, int current);
-std::string GetString(const std::string &s, int start);
-bool IsNum(char ch);
-std::pair<double, int> GetNumWithLen(const std::string &s, int start); 
+bool isSpace(char ch);
+int findEnd(const std::string& s, int startInd, char openSym, char closeSym);
+int missSpaces(const std::string&s, int current);
+std::string getString(const std::string &s, int start);
+bool isNum(char ch);
+std::pair<double, int> getNumWithLen(const std::string &s, int start); 
